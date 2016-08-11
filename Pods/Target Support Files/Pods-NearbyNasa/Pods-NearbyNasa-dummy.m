b890c37ed46efa59c0e612023c00e904fc6c5416
Johann Kerr <johannkerr@gmail.com>
2016-08-10 20:05:05 -0400

    second commit

---
+++ Pods/Target Support Files/Pods-NearbyNasa/Pods-NearbyNasa-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NearbyNasa : NSObject
+@end
+@implementation PodsDummy_Pods_NearbyNasa
+@end
---
+++ Pods/Target Support Files/Pods-NearbyNasa/Pods-NearbyNasa-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_NearbyNasaVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_NearbyNasaVersionString[];
+
